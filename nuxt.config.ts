b93962dfddcfc1e81c92aa84a54f82b9d5cc949c
razmatinyan <razmatinyan28@gmail.com>
2026-07-17 11:33:44 +0400

    chore: add deps and update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	modules: [
+		'@razmatinyan/nuxt-email',
+		'@nuxtjs/i18n',
+		'@nuxt/fonts',
+		'@nuxt/icon',
+		'@nuxtjs/tailwindcss',
+	],
+	email: {
+		provider: 'console',
+		from: 'App <noreply@myapp.com>',
+	},
+	vite: {
+		optimizeDeps: {
+			include: ['@vue/devtools-core', '@vue/devtools-kit'],
+		},
+	},
+	i18n: {
+		locales: [
+			{ code: 'en', iso: 'en-US', file: 'en.json', name: 'English' },
+			{ code: 'ru', iso: 'ru-RU', file: 'ru.json', name: 'Русский' },
+			{ code: 'hy', iso: 'hy-AM', file: 'hy.json', name: 'Հայերեն' },
+		],
+		defaultLocale: 'hy',
+		strategy: 'prefix_and_default',
+		customRoutes: 'config',
+		detectBrowserLanguage: {
+			useCookie: true,
+		},
+	},
 })
